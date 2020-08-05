6df0730b498880e4ef239d9d7e112a14674d6da8
Mike Brown <mike@mostlysafe.com>
2020-08-05 14:03:27 -0500

    initial commit, spring initializr

---
+++ src/test/java/com/mostlysafe/docservice/DocserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mostlysafe.docservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
